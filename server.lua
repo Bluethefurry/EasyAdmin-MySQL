fa06562ff827cc7e75f4f7994f3a05238bf1a348
Eric Mager <iamabluewolf@gmail.com>
2019-12-09 21:47:16 +0100

    make it work

--- server.lua
+++ server.lua
@@ -1,57 +1,20 @@
 local dbReady = false
-local tableQuery = "CREATE TABLE IF NOT EXISTS `ea_bans`( `banid` int(11) NOT NULL UNIQUE AUTO_INCREMENT, `expire` double NOT NULL DEFAULT '10444633200', `identifiers` text NOT NULL, `reason` text NOT NULL, PRIMARY KEY(`banid`)) "
-
-updateScripts = {
-	"ALTER TABLE `ea_bans` ADD COLUMN `discord` text NOT NULL",
-	"Truncate table `ea_bans`; ALTER TABLE `ea_bans` DROP COLUMN `discord`, DROP `steam`, DROP `identifier`, ADD COLUMN `identifiers` text NOT NULL"
-}
-
-
-local cachedBans = {} -- DO NOT TOUCH
+local supportedVersion = 5.12
+local cachedBans = {} -- DO NOT TOUCH THIS EVER DONT EDIT THIS
 
 AddEventHandler('onMySQLReady', function ()
-	MySQL.Async.execute(tableQuery, {}, function() end)
-	print("executed table query")
+	Wait(200)
 
-	-- perform upgrades if necesarry
-
-	MySQL.Async.execute("SELECT count(*) FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'identifiers' and TABLE_NAME = 'ea_bans'", {}, function(count)
-		if count == 0 then
-			Citizen.Trace("Performin Database Upgrade...")
-			local fetchedAllBans = false
-			MySQL.Async.fetchAll('SELECT * FROM ea_bans', {}, function(bans)
-				cachedBans = bans
-				fetchedAllBans = true
-				print("retrieved banlist")
-			end)
-			repeat
-				Wait(500)
-			until fetchedAllBans
-			MySQL.Async.execute(updateScripts[2], {}, function() end)
-			for i, ban in ipairs(cachedBans) do
-				ban.identifiers = {}
-				if ban.identifier then
-					table.insert(ban.identifiers, ban.identifier)
-					ban.identifier = nil
-				end
-				if ban.steam then
-					table.insert(ban.identifiers, ban.steam)
-					ban.steam = nil
-				end
-				if ban.discord then
-					table.insert(ban.identifiers, ban.discord)
-					ban.discord = nil
-				end
-				TriggerEvent("ea_data:addBan", ban)
-			end
-			print("Performed ea_bans Database Upgrade, no further action is necesarry.")
+	TriggerEvent("EasyAdmin:GetVersion", function(version)
+		if tonumber(version) >= supportedVersion then
+			dbReady = true
+		else
+			print("EasyAdmin-MySQL: Update EasyAdmin, it's outdated and incompatible.")
 		end
 	end)
-	
-	
-	Wait(100)
-	dbReady = true
+
 end)
+
 
 AddEventHandler('ea_data:retrieveBanlist', function(callback)
 	local callback = callback
@@ -72,7 +35,15 @@ AddEventHandler('ea_data:addBan', function(data)
 	while not dbReady do
 		Wait(1000)
 	end
-	MySQL.Async.execute("INSERT INTO ea_bans (`banid`, `expire`, `identifiers`, `reason`) VALUES (NULL, @expire, @identifiers, @reason);", {expire = data.expire, identifiers = json.encode(data.identifiers), reason = data.reason }, function() end)
+	MySQL.Async.execute("INSERT INTO ea_bans (`banid`, `expire`, `identifiers`, `reason`, `banner`) VALUES (NULL, @expire, @identifiers, @reason, @banner);", {banid = data.banid, expire = data.expire, identifiers = json.encode(data.identifiers), reason = data.reason, banner = data.banner or "Console" }, function() end)
+	print("added new ban")
+end)
+
+AddEventHandler('ea_data:updateBan', function(data)
+	while not dbReady do
+		Wait(1000)
+	end
+	MySQL.Async.execute("UPDATE `ea_bans` SET `expire`=@expire, `identifiers`=@identifiers, `reason`=@reason, banner=@banner WHERE banid=@banid;", {banid = data.banid, expire = data.expire, identifiers = json.encode(data.identifiers), reason = data.reason, banner = data.banner or "Console" }, function() end)
 	print("added new ban")
 end)
 
@@ -80,15 +51,12 @@ AddEventHandler('ea_data:removeBan', function(data)
 	while not dbReady do
 		Wait(1000)
 	end
+	print(data.banid)
 	for i,theBan in ipairs(cachedBans) do
-		for index,identifier in ipairs(theBan.identifiers) do
-			for d, dataidentifier in ipairs(data.identifiers) do
-				if dataidentifier == identifier then
-					MySQL.Async.execute("DELETE FROM ea_bans WHERE banid = @banid;", {banid = theBan.banid }, function() end)
-					print("deleted ban")
-					break
-				end
-			end
+		if data.banid == theBan.banid then
+			MySQL.Async.execute("DELETE FROM ea_bans WHERE banid = @banid;", {banid = theBan.banid }, function() end)
+			print("deleted ban")
+			break
 		end
 	end
 end)
